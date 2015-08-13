8a302f9d750ceaad03a34f2605e7f4bb9fae8fe7
miloas <0x9357@gmail.com>
2015-08-13 18:09:36 +0800

    change name

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject colorstr "0.1.0"
   :description "A Clojure library to stylized string in console using ANSI escape code."
-  :url "http://github.com/miloas/colorful-string"
+  :url "http://github.com/miloas/colorstr"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
